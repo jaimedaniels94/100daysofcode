09a9b8db7b027a6c537db8084402969b9fb4031f
jaimedaniels94 <jaimedaniels94@gmail.com>
2021-07-06 13:15:58 -0500

    Day 3 exercise 5.... COMPLETE

--- day3/ex-5.py
+++ day3/ex-5.py
@@ -2,6 +2,35 @@
 #Starter code:
 print("Welcome to the love calculator!")
 name1 = input("What is your name? \n")
-name 2 = input("What is their name? \n")
+name2 = input("What is their name? \n")
 #End starter code. 
 
+lower_case_name1 = name1.lower()
+lower_case_name2 = name2.lower()
+
+true_points = name1.count("t")
+true_points += name2.count("t")
+true_points += name1.count("r")
+true_points += name2.count("r")
+true_points += name1.count("u")
+true_points += name2.count("u")
+true_points += name1.count("e")
+true_points += name2.count("e")
+
+love_points = name1.count("l")
+love_points += name2.count("l")
+love_points += name1.count("o")
+love_points += name2.count("o")
+love_points += name1.count("v")
+love_points += name2.count("v")
+love_points += name1.count("e")
+love_points += name2.count("e")
+
+score = str(true_points) + str(love_points)
+
+if int(score) < 10 or int(score) > 90:
+  print(f"Your score is {score}. You go together like mentos and coke. Explosive.")
+elif int(score) >= 40 and int(score) <= 50:
+  print(f"Your score is {score}, you are alright together.")
+else:
+  print(f"Your score is {score}, you're probably not gonna last. Sorry")
