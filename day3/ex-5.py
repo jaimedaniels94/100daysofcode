bba1fb1751a68f17c9118c15c2e28a8522d74ccb
jaimedaniels94 <jaimedaniels94@gmail.com>
2021-07-06 12:33:46 -0500

    starter code, day 3 exercise 5.

---
+++ day3/ex-5.py
@@ -0,0 +1,7 @@
+#Build a program that calculates love connection based on how many letters of the subjects' names occure in TRUE and LOVE
+#Starter code:
+print("Welcome to the love calculator!")
+name1 = input("What is your name? \n")
+name 2 = input("What is their name? \n")
+#End starter code. 
+
