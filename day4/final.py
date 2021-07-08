d8359710b232904932e06589dfd07eb916285720
jaimedaniels94 <jaimedaniels94@gmail.com>
2021-07-08 10:53:00 -0500

    changed up logic in order to debug.

--- day4/final.py
+++ day4/final.py
@@ -29,21 +29,25 @@ scissors = '''
 
 ascii = [rock, paper, scissors]
 print("READY TO ROCK, PAPER, SCISSORS?")
+
 player_num = int(input("Choose your weapon. \n0 for rock, 1 for paper, or 2 for scissors.\n"))
-print(f"You chose:\n{ascii[player_num]}")
-computer_num = random.randint(0, 2)
-print(f"Computer chose:\n{ascii[computer_num]}")
 
-
-if player_num == 0 and computer_num == 2:
-  print("You win!")
-elif player_num == 2 and computer_num == 1:
-  print("You win!")
-elif player_num == 1 and computer_num == 0:
-  print("You win!")
-elif player_num == computer_num:
-  print("It's a draw. Play again.")
-elif player_num > 2:
+if player_num > 2 or player_num < 0:
   print("Invalid entry. Play again.")
-else:
-  print("You lose. Hahah you lost to a computer.")
+else: 
+  print(f"You chose:\n{ascii[player_num]}")
+
+  computer_num = random.randint(0, 2)
+  print(f"Computer chose:\n{ascii[computer_num]}")
+
+
+  if player_num == 0 and computer_num == 2:
+    print("You win!")
+  elif player_num == 2 and computer_num == 1:
+    print("You win!")
+  elif player_num == 1 and computer_num == 0:
+    print("You win!")
+  elif player_num == computer_num:
+    print("It's a draw. Play again.")
+  else:
+    print("You lose. Hahah you lost to a computer.")
