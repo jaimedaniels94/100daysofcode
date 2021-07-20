ed7bac75acdf68ba9d12c28b1ef4bc6274560cac
jaimedaniels94 <jaimedaniels94@gmail.com>
2021-07-20 12:16:36 -0500

    cleaned up

--- day7/hangman.py
+++ day7/hangman.py
@@ -1,73 +1,15 @@
 import random
+from hangman_art import logo, stages
+from hangman_words import word_list
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+
 
 end_of_game = False
-word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 lives = 6
 
-#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
-#Set 'lives' to equal 6.
-
-# Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+print(logo)
 
 # Create blanks
 display = []
@@ -78,25 +20,24 @@ for l in range(word_length):
 while not end_of_game:
     guess = input("Guess a letter:\n").lower()
 
+    if guess in display:
+      print(f"You've already guessed {guess}")
     #Check guessed letter
     for position in range(word_length):
       letter = chosen_word[position]
       if letter == guess:
         display[position] = letter
       
-    #TODO-2: - If guess is not a letter in the chosen_word,
-    #Then reduce 'lives' by 1. 
-    #If lives goes down to 0 then the game should stop and it should print "You lose."
+
     if guess not in chosen_word:
       lives -= 1
       if lives == 0:
         end_of_game = True
         print("You lose.")
-        
+
     print(f"{' '.join(display)}")
     
     if "_" not in display:
       end_of_game = True
     
-    #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
     print(f"{stages[lives]}")
